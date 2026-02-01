2d2f4dce2d0c11f2d4f7f6f604bbe0c4b69ac611
ausafelahi <ausafelahi8@gmail.com>
2026-02-01 16:05:01 +0500

    Add Auth Page with Login and Sign Up Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Home from "./pages/home/Home";
 import NoteEditor from "./pages/notes/NoteEditor";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 
 function App() {
+  const location = useLocation();
+
+  const authRoutes = ["/login", "/signup"];
+  const isAuthRoute = authRoutes.includes(location.pathname);
+
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/notes/:id" element={<NoteEditor />} />
-      </Routes>
-    </Layout>
+    <>
+      {isAuthRoute ? (
+        <Routes>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      ) : (
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/notes/:id" element={<NoteEditor />} />
+          </Routes>
+        </Layout>
+      )}
+    </>
   );
 }
 
--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -9,7 +10,9 @@ const Header = () => {
           <img src="/Nodus.png" alt="Nodus" height="auto" width="120px" />
           <div className="flex items-center gap-6 px-12">
             <ModeToggle />
-            <Button className="bg-teal-500 hover:bg-teal-800">Sign In</Button>
+            <Link to="/login">
+              <Button className="bg-teal-500 hover:bg-teal-800">Sign In</Button>
+            </Link>
             <Button className="bg-teal-500 hover:bg-teal-800">Sign Up</Button>
           </div>
         </div>
